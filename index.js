1965a0d4d88afc8d74be02e3050a8c0df2db61de
shirrie01 <shirrie.01@gmail.com>
2026-01-09 23:48:51 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("BizGenie Cloud Run is up");
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Listening on", port);
+});
