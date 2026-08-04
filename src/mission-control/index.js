7b71015676536368087e1af5fe3f9266f2444b9a
shirrie01 <shirrie.01@gmail.com>
2026-08-04 15:55:05 +0100

    BG-MC-002: Add Mission Control intelligence schemas (#9)
    * Document Mission Control intelligence contracts
    
    * Add Mission Control intelligence schemas
    
    * Test Mission Control intelligence schemas
    
    * Export Mission Control intelligence schemas
    
    * Normalize Mission Control intelligence schema files

--- src/mission-control/index.js
+++ src/mission-control/index.js
@@ -3,11 +3,13 @@ const {
   MissionControlRepository,
 } = require("./repository");
 const { createMissionControlRouter } = require("./router");
+const intelligenceSchemas = require("./intelligence-schemas");
 const schemas = require("./schemas");
 
 module.exports = {
   InMemoryMissionControlRepository,
   MissionControlRepository,
   createMissionControlRouter,
+  ...intelligenceSchemas,
   ...schemas,
 };
