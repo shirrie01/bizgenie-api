9893037af11c978fc4af11fb5b4eeabda08c289e
shirrie01 <shirrie.01@gmail.com>
2026-08-04 15:49:09 +0100

    Export Mission Control intelligence schemas

--- src/mission-control/index.js
+++ src/mission-control/index.js
@@ -3,11 +3,14 @@ const {
   MissionControlRepository,
 } = require("./repository");
 const { createMissionControlRouter } = require("./router");
+const intelligenceSchemas = require("./intelligence-schemas");
 const schemas = require("./schemas");
 
 module.exports = {
   InMemoryMissionControlRepository,
   MissionControlRepository,
   createMissionControlRouter,
+  ...intelligenceSchemas,
   ...schemas,
 };
+
